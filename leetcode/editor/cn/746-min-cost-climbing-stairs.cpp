cacfec0d611ac00271ab4554146aec426ed6fad6
frankcbliu <641259875@qq.com>
2023-08-22 15:21:50 +0800

    algorithm: 动态规划 2刷

--- leetcode/editor/cn/746-min-cost-climbing-stairs.cpp
+++ leetcode/editor/cn/746-min-cost-climbing-stairs.cpp
@@ -49,7 +49,19 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    int minCostClimbingStairs1(vector<int> &cost) {
+    // 二刷
+    int minCostClimbingStairs(vector<int> &cost) {
+        int n = cost.size();
+        // 1. dp[i] 到达 第 i 级台阶需要花费的最小金额
+        vector<int> dp(n + 1);
+        // 2. dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]cost[i-2]);
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[n];
+    }
+
+    int minCostClimbingStairs2(vector<int> &cost) {
         size_t n = cost.size();
         if (n <= 1) return 0;
         // 1. dp[n] 到达第 n 级台阶，所花费的最小金额
@@ -68,7 +80,7 @@ public:
     }
 
     //    优化内存版本
-    int minCostClimbingStairs(vector<int> &cost) {
+    int minCostClimbingStairs1(vector<int> &cost) {
         size_t n = cost.size();
         if (n <= 1) return 0;
         int dp0, dp1, res;
