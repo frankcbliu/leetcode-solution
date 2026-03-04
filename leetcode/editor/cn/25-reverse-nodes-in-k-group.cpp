5eb9378f32755c76bd185f6013885886ba6da085
frankcbliu <641259875@qq.com>
2026-03-04 11:13:02 +0800

    feat: update

--- leetcode/editor/cn/25-reverse-nodes-in-k-group.cpp
+++ leetcode/editor/cn/25-reverse-nodes-in-k-group.cpp
@@ -55,87 +55,47 @@
  * };
  */
 class Solution {
-public:
-    ListNode *reverseKGroup(ListNode *head, int k) {
-        if (k < 2) {// 不翻转
-            return head;
-        }
-        // 构建 left 节点
-        ListNode *left = new ListNode();
-        left->next = head;
-        // 构建 cur 节点
-        ListNode *cur = head;
-        int cnt = 0;
-        while (cur != nullptr) {
-            ++cnt;
-            // 取模， 比如 k = 3, cnt=1, 1%k=1; cnt=2, k%2=2; cnt=3, k%3=0,符合条件
-            if (cnt % k == 0) {
-                left = reverse(left, left->next, cur, cur->next);
-                // 首次翻转时，上面 left = begin = head, 因此, head 需要更新为 cur, 而后续不用
-                head = (head == left ? cur : head);
-                cur = left;
-            }
-            cur = cur->next;
-        }
-        return head;
+  public:
+  // 解法：灵府山
+  ListNode *reverseKGroup(ListNode *head, int k) {
+    // 计算链表长度
+    int n = 0;
+    ListNode *cur = head;
+    while (cur != nullptr) {
+      n++;
+      cur = cur->next;
     }
-
-
-    // 翻转 left -> begin -> ... -> end -> right
-    // 变成 left -> end -> ... -> begin -> right
-    // 注意，返回的并不是翻转后的头节点，而是翻转后的尾节点
-    ListNode *reverse(ListNode *left, ListNode *begin, ListNode *end, ListNode *right) {
-        ListNode *prev = left;
-        ListNode *cur = begin;
-        while (cur != right) {
-            ListNode *tmp = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = tmp;
-        }
-        left->next = end;
-        begin->next = right;
-        return begin;
+    // 哨兵节点
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode *p0 = &dummy;
+    ListNode *pre = nullptr;
+    cur = p0->next;
+    // K个一组进行反转, 不足k个的不反转
+    while (n >= k) {
+      n -= k;
+      for (int i = 0; i < k; i++) {
+        ListNode *nxt = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = nxt;
+      }
+      // 记录哨兵节点的下一个位置
+      ListNode *nxt2 = p0->next;
+      p0->next->next = cur;
+      p0->next = pre;
+      p0 = nxt2;
     }
-
-    // 解法2：灵府山
-    ListNode *reverseKGroup2(ListNode *head, int k) {
-        // 计算链表长度
-        int n = 0;
-        ListNode *cur = head;
-        while (cur != nullptr) {
-            n++;
-            cur = cur->next;
-        }
-        // 哨兵节点
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode *p0 = &dummy;
-        ListNode *pre = nullptr;
-        cur = p0->next;
-        // K个一组进行反转, 不足k个的不反转
-        while (n >= k) {
-            n -= k;
-            for (int i = 0; i < k; i++) {
-                ListNode *nxt = cur->next;
-                cur->next = pre;
-                pre = cur;
-                cur = nxt;
-            }
-            ListNode *nxt2 = p0->next;
-            p0->next->next = cur;
-            p0->next = pre;
-            p0 = nxt2;
-        }
-        return dummy.next;
-    }
+    // 返回头节点
+    return dummy.next;
+  }
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
 
 int main() {
-    Solution s;
-    vector<int> arr{7, 1, 5, 3, 6, 4};
-    auto res = s.twoSum(arr, 11);
-    showVector(res);
+  Solution s;
+  vector<int> arr{7, 1, 5, 3, 6, 4};
+  auto res = s.twoSum(arr, 11);
+  showVector(res);
 }
